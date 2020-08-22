21d3e86cbc1362d89a1009140c3d931977f433d1
KRIDSAYA NATEEMONTON <63010026@KMITL.AC.TH>
2020-08-23 06:55:44 +0700

    แยกตัวประกอบ

--- lab02 - 01/lab02-01.cpp
+++ lab02 - 01/lab02-01.cpp
@@ -2,12 +2,14 @@
 int main()
 {
     int a, n = 2;
+    printf("Enter number :");
     scanf_s("%d", &a);
+    printf("Factoring Result :");
     while (a != 1)
     {
         while (a % n == 0)
         {
-            printf("%d\n", n);
+            printf("%d ", n);
             a = a / n;
         }
         n++;
